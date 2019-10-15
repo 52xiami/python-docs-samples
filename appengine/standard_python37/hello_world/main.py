a9fc6f315e8385729668840a04ff592a8eb7f5ec
Jun Zhang <47554383+52xiami@users.noreply.github.com>
2019-10-14 22:31:53 -0700

    Update main.py

--- appengine/standard_python37/hello_world/main.py
+++ appengine/standard_python37/hello_world/main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World!'
+    return 'CMPE281 Cloud Computing!'
 
 
 if __name__ == '__main__':
